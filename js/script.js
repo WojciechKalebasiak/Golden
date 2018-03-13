7dc3a7312d239bd6c9dcf7a4a1dd7ebd7af63124
Wojciech Kalebasiak <kalebasiakw@gmail.com>
2018-03-13 02:29:26 +0100

    Disable animations on mobile devices

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 $(function() {
     //Init Animate on Scroll efects
-    AOS.init();
+    AOS.init({
+    	disable:'mobile'
+    });
     // Animating header
     $('body').hide();
     $('.tellmemore').hide();
     setTimeout(function() {
         $('body').fadeIn('100');
-    }, 500);
+    }, 500);	
     setTimeout(function() {
         $('.tellmemore').jAnimateOnce('fadeInUp').show();
     }, 2500);
